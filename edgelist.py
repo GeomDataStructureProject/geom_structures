e00b1c2886e31efe0e5ce0300c551025b7aac491
Connor Fogarty <fogart@stolaf.edu>
2023-01-23 14:04:40 -0600

    edgy

--- edgelist.py
+++ edgelist.py
@@ -1,1 +1,16 @@
+class vertex:
+    def __init__(self, point, halfEdge):
+        self.point = point
+        self.halfEdge = halfEdge
 
+class halfEdge:
+    def __init__(self, point1, point2, face, direction):
+        self.point1 = point1
+        self.point2 = point2
+        self.face = face
+        self.direction = direction
+
+class face:
+    def __init__(self, halfEdge1, halfEdge2):
+        self.halfEdge1 = halfEdge1
+        self.halfEdge2 = halfEdge2
