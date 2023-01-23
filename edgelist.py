bb65c4b28fb9fb65125606f37e1462dc878ddaf1
Ryan Harvey <harvey6@stolaf.edu>
2023-01-23 13:43:21 -0600

    some changes

--- edgelist.py
+++ edgelist.py
@@ -1,1 +1,19 @@
 
+
+class Vertex:
+    '''Vertex class'''
+    def __init__(self, coordinates_tuple, adjacent_half_edge):
+        self.coord = coordinates_tuple
+        self.half_edge = adjacent_half_edge
+    
+    def print(self):
+        '''Prints coordinates'''
+        print(self.coord[0][0])
+        print(self.coord[0][1])
+
+
+
+a = Vertex((1,2), 'test')
+a.print()
+    
+
