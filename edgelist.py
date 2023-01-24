f0b7b7d0d74d2c0a247ea9d48d1a664472a12323
Dylan <dylaneugenethomas@gmail.com>
2023-01-24 14:30:51 -0600

    Tuesday Changes

--- edgelist.py
+++ edgelist.py
@@ -1,43 +1,104 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+
 #pylint: disable = line-too-long, too-many-lines, no-name-in-module, import-error, multiple-imports, pointless-string-statement, wrong-import-order, invalid-name, missing-function-docstring, missing-class-docstring, consider-using-enumerate
-class vertex:
+class Vertex:
     '''Vertex class, stores an x, y value, and pointer list of adjacent halfedges'''
-    def __init__(self, point, adj_halfEdge):
+    def __init__(self, point):
         self.point = point
-        self.halfEdgeList = adj_halfEdge
-    def print(self):
-        print(self.point)
+        self.halfEdgeList = 0
+    def getX(self):
+        return self.point[0]
+    def getY(self):
+        return self.point[1]
 
-class halfEdge:
+class HalfEdge:
     '''halfEdge class, stores pointers to: adjacent face, its dual, next and previous half edge, and its origin point '''
-    def __init__(self, adj_face, dual_half_edge, next_half_edge, prev_half_edge, origin_point):
-        self.face = [adj_face]
-        self.dual = [dual_half_edge]
-        self.next = [next_half_edge]
-        self.prev = [prev_half_edge]
-        self.origin = [origin_point]
-
+    def __init__(self, origin_point):
+        self.face = 0
+        self.dual = 0
+        self.next = 0
+        self.prev = 0
+        self.origin = origin_point
+    
+    def setFace(self, face):
+        self.face = face
     def setDual(self, dual):
-        self.dual = [dual]
+        self.dual = dual
+    def setNext(self, next):
+        self.next = next
+    def setPrev(self, prev):
+        self.prev = prev
 
-class face:
+class Face:
     '''Face class, stores list of all adjacent halfedges'''
     def __init__(self, half_edge_list):
         self.halfEdgeList = half_edge_list
 
 
+# random point generation
+def randPoints(numPoints, low, high):
+    points = []
+
+    for i in range(numPoints):
+        points.append(Vertex((random.randint(low, high), random.randint(low, high)), 0))
+
+    return points
+    
+# plotting points
+def addToPlot(points):
+    pointsXY = [*zip(*points)] # turns points into two lists, one for x and another for y
+    pointsXY = [np.array(pointsXY[0]), np.array(pointsXY[1])]
+    plt.scatter(pointsXY[0], pointsXY[1])
+
+    # Add on last coordinate to the end
+    pointsXY[0] = np.append(pointsXY[0], pointsXY[0][0]) # add X coordinate
+    pointsXY[1] = np.append(pointsXY[1], pointsXY[1][0]) # add Y coordinate
+    plt.plot(pointsXY[0], pointsXY[1])
+
+def sortByX(points):
+    swapped = False # used for deterining whether to exit loop permaturely
+
+    for i in range(len(points) - 1):
+        for j in range(len(points) - i - 1):
+            if points[j+1] < points[j]:
+                swapped = True
+                points[j], points[j+1] = points[j+1], points[j]
+        if not swapped:
+            break
+
+    return points
+    
+def leftOf(a, b, c):
+    signedArea = (b.getX() - a.getX())*(c.getY() - a.getY()) - (b.getY() - a.getY())*(c.getX() - a.getX())
+    return signedArea > 0
+
+def findVisible(p, edgeList):
+    '''Returns all visible points to the left from given point, idk how to do this'''
+    
+
+def incrementalTriangulate(points):
+    '''Given a list of sorted points, returns Double edge list of the incremental triangulation of the points'''
+    vertex_list = [points[0], points[1], points[2]]
+    double_edge_list = [HalfEdge(points[0]), HalfEdge(points[1]), HalfEdge(points[2])]
+    
+    for i in range(points):
+        visible_points = findVisible(i, )
+        for j in range(visible_points()):
+            
+pts = randPoints(10, -10, 10)
+pts = sortByX(pts)
 
 
-a = vertex((1,2), 0)
-b = vertex((3,2), 0)
+
+
+a = Vertex((1,2), 0)
+b = Vertex((3,2), 0)
 b.print()
 
-half_a = halfEdge(0, 0, 0, 0, b)
-half_b = halfEdge(0, half_a, 0, 0, a)
+half_a = HalfEdge(0, 0, 0, 0, b)
+half_b = HalfEdge(0, half_a, 0, 0, a)
 half_a.setDual(half_b)
 
 half_a.dual[0].origin[0].print()
-
-a = [1]
-b = a
-a[0] = 2
-print(b[0])
